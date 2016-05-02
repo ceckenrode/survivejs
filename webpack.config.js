90b69d113ab3fe92b5de4b6f3f5484f38daa701a
Christian Eckenrode Work <christianeckenrodework@Christians-MacBook-Pro.local>
2016-05-01 21:05:13 -0400

    updated webpack.config.js for hotmodulereplacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,59 @@
-var path = require('path');
+const path = require('path');
 
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
-};
+}
 
-module.exports = {
+const common = {
   //Entry accepts a path or an object of entries. We'll be using the
   //later form given it's convient with more complex configurations
+  entry: {
+    app: PATHS.app,
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  }
+};
+//Default configuration. We will return this if
+//Webpack is called outside of npm.
+
+if (TARGET === 'start' || !TARGET){
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+
+      //Enable history API fallback so HTML5 History API based
+      //routing works. This is a good default that will come in
+      //handy in more complicated setups
+      historyAPIFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      //Display only errors to reduce the ammount of output
+      stats: 'errors-only',
+
+      //Parse host and port from env so this is easy to customize.
+      //
+      //If you use Vagrant or Cloud9, set
+      // host: process.env.HOST || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices unlike Default
+      //localhost
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if (TARGET === 'build'){
+  module.exports = merge(common, {});
 }
